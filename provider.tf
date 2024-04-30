a5e84fe40d08316cc090a0ed07aa2f6b79d4e305
counain <syedcounain143@gmail.com>
2024-04-30 12:14:37 +0000

    new commit

--- provider.tf
+++ provider.tf
@@ -1,8 +1,7 @@
 provider "aws" {
- region = "ap-south-1"
- access_key = "AKIA2CCD3R2CNTEVMH56"
- secret_key = "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"
+  region = "ap-south-1"
 }
+
 
 terraform {
  required_version = "~>1.8.0"
